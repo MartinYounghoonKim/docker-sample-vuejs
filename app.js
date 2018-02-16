d377462fd1f4ffb4008086cf5586683f37962015
martinyounghoonkim <hoons0131@gmail.com>
2018-02-16 15:12:44 +0900

    Setting for express app

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const engine = require('ejs-locals');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+app.engine('html', engine);
+
+const route = (() => {
+  app.get('/', (req, res) => {
+    res.render('index.html');
+  })
+})();
+
+let server = app.use(express.static('public')).listen(port, () => {
+  console.log(`Express server has started on port:${port}`);
+});
