379d9e7b6cd6eb9c1707a74954755e9f6c2f8c25
martinyounghoonkim <hoons0131@gmail.com>
2018-02-17 23:18:38 +0900

    Testing injection webpack middleware in express server entry file

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 4000;
 const engine = require('ejs-locals');
 const bodyParser = require('body-parser');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpackDevConfig = require('./build/webpack.dev.conf.js');
+console.log(webpackDevConfig);
+//const compiler = webpack(webpackDevConfig);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', __dirname);
@@ -14,6 +20,13 @@ const route = (() => {
     res.render('dist/index.html');
   })
 })();
+
+// if(process.env.NODE_ENV !== 'production') {
+//   app.use(webpackDevMiddleware(compiler, {
+//     noInfo: true
+//   }));
+//   app.use(webpackHotMiddleware(compiler));
+// }
 
 let server = app.use(express.static('dist')).listen(port, () => {
   console.log(`Express server has started on port:${port}`);
