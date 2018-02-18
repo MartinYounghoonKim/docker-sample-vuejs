3d60ab7d5ef83d712d8069d8875c3a31482367f5
martinyounghoonkim <hoons0131@gmail.com>
2018-02-18 18:25:07 +0900

    Update webpack moduler

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const bodyParser = require('body-parser');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
-const webpackDevConfig = require('./build/webpack.dev.conf.js');
+const webpackDevConfig = require('./build/webpack.base.conf.js');
 console.log(webpackDevConfig);
-//const compiler = webpack(webpackDevConfig);
+const compiler = webpack(webpackDevConfig);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', __dirname);
@@ -21,12 +21,12 @@ const route = (() => {
   })
 })();
 
-// if(process.env.NODE_ENV !== 'production') {
-//   app.use(webpackDevMiddleware(compiler, {
-//     noInfo: true
-//   }));
-//   app.use(webpackHotMiddleware(compiler));
-// }
+if(process.env.NODE_ENV !== 'production') {
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true
+  }));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 let server = app.use(express.static('dist')).listen(port, () => {
   console.log(`Express server has started on port:${port}`);
