6ccf1533a9309f5c363c3eab86d6ba32b5a70aa5
martinyounghoonkim <hoons0131@gmail.com>
2018-02-18 18:26:32 +0900

    Add webpack-dev-server's config

---
+++ build/webpack-dev.server.config.js
@@ -0,0 +1,70 @@
+var webpack = require('webpack');
+
+module.exports = {
+
+  entry: [
+    './src/main.js',
+    'webpack-dev-server/client?http://0.0.0.0:3001',
+    'webpack/hot/only-dev-server'
+  ],
+
+  output: {
+    path: '/',
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    hot: true,
+    filename: 'bundle.js',
+    publicPath: '/',
+    historyApiFallback: true,
+    contentBase: './public',
+    proxy: {
+      "**": "http://localhost:3000"
+    }
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderConfig
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  },
+};
