2da54de92c30fd1539b33ac4008ba4c29d196fd8
martinyounghoonkim <hoons0131@gmail.com>
2018-02-19 09:30:48 +0900

    Add webpack hot module in express entry file

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express();
-const port = 4000;
+const http = require('http');
 const engine = require('ejs-locals');
 const bodyParser = require('body-parser');
 const webpack = require('webpack');
 const hotMiddleware = require('./build/webpack-dev.server.config');
 const webpackDevConfig = require('./build/webpack.base.conf.js');
-console.log(webpackDevConfig);
+const port = 4000;
 const compiler = webpack(webpackDevConfig);
+
+const app = express();
+const server = http.createServer(app);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', __dirname);
@@ -25,7 +27,8 @@ const route = (() => {
 if(process.env.NODE_ENV !== 'production') {
   app.use(hotMiddleware);
 }
+app.use(express.static('dist'));
 
-let server = app.use(express.static('dist')).listen(port, () => {
+server.listen(port, () => {
   console.log(`Express server has started on port:${port}`);
 });
