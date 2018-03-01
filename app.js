a91dccc37172362eca844a0651ceffd684cb7bf8
martinyounghoonkim <hoons0131@gmail.com>
2018-03-02 00:50:17 +0900

    Update express moduler

--- app.js
+++ app.js
@@ -1,49 +1,81 @@
+/**
+ * @description express에 필요한 모듈 추가
+ */
 const express = require('express');
+const path = require('path');
+
+const app = express();
+/**
+ * @description 개발/상용에 따라 설정값 분기
+ */
+const isProduction = process.env.NODE_ENV === 'production';
+const port = isProduction ? process.env.PORT : 4000
+
+const publicPath = path.resolve(__dirname, 'dist');
+
 const http = require('http');
 const engine = require('ejs-locals');
 const bodyParser = require('body-parser');
 
-const port = 4000;
-
-const app = express();
 const server = http.createServer(app);
 
+app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
 app.engine('html', engine);
+/**
+ *
+ * @type {webpack}
+ */
+const webpack = require('webpack');
 
 
-const route = (() => {
-  app.get('/', (req, res) => {
-    console.log('정보', process.env.NODE_ENV);
-    res.render('index.html')
-    // if(process.env.NODE_ENV !== 'production') {
-    //   res.render('/dist/index.html')
-    // } else {
-    //   res.render('/index.html')
-    // }
-  })
-})();
+
+// const route = (() => {
+//   app.get('/', (req, res) => {
+//     console.log('정보', process.env.NODE_ENV);
+//     res.render('index.html')
+//     // if(process.env.NODE_ENV !== 'production') {
+//     //   res.render('/dist/index.html')
+//     // } else {
+//     //   res.render('/index.html')
+//     // }
+//   })
+// })();
 
 if(process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack');
-  const config = require('./build/webpack-dev.server.config');
+  const devConfig = require('./build/webpack.dev.conf');
   const webpackDevConfig = require('./build/webpack.base.conf.js');
   const webpackDevMiddleware = require('webpack-dev-middleware');
   const webpackHotMiddleware = require('webpack-hot-middleware');
   const compiler = webpack(webpackDevConfig);
 
-  app.use(webpackDevMiddleware(compiler, {
+  const middleware = webpackDevMiddleware(compiler, {
     noInfo: true,
-    publicPath: 'http://localhost:4000'
-  }));
-  app.use(webpackHotMiddleware(compiler, {
-    log: console.log
-  }))
-  app.use(config);
+    publicPath: 'http://localhost:4000',
+    quiet: false,
+    lazy: false,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true
+    },
+    stats: {
+      colors: true,
+    }
+  });
+
+  const bundlePath = path.join(__dirname, 'dist/index.html');
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+
+  app.get('*', function response(req, res) {
+    res.render('index.html')
+    res.end();
+  });
+
+
 }
-app.use(express.static('dist'));
 
 server.listen(port, () => {
   console.log(`Express server has started on port:${port}`);
