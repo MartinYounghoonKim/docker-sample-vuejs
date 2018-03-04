5fcc197eee1f0de7eb23e3cdac7f073df9ddac83
martinyounghoonkim <hoons0131@gmail.com>
2018-03-04 11:39:02 +0900

    Edit express server's moduler

--- app.js
+++ app.js
@@ -13,84 +13,21 @@ const port = isProduction ? process.env.PORT : 4000
 
 const publicPath = path.resolve(__dirname, 'dist');
 
-const http = require('http');
-const engine = require('ejs-locals');
-const bodyParser = require('body-parser');
+if (!isProduction) {
+  const bundle = require('./server/bundle.js');
+  bundle();
 
-const server = http.createServer(app);
-
-app.use(express.static(publicPath));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.set('views', __dirname);
-app.set('view engine', 'ejs');
-app.engine('html', engine);
-/**
- *
- * @type {webpack}
- */
-const webpack = require('webpack');
-
-
-
-// const route = (() => {
-//   app.get('/', (req, res) => {
-//     console.log('정보', process.env.NODE_ENV);
-//     res.render('index.html')
-//     // if(process.env.NODE_ENV !== 'production') {
-//     //   res.render('/dist/index.html')
-//     // } else {
-//     //   res.render('/index.html')
-//     // }
-//   })
-// })();
-
-if(process.env.NODE_ENV !== 'production') {
-  const devConfig = require('./build/webpack.dev.conf');
-  const webpackDevConfig = require('./build/webpack.base.conf.js');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
-  const compiler = webpack(webpackDevConfig);
-
-  const middleware = webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: 'http://localhost:4000',
-    quiet: false,
-    lazy: false,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: true
-    },
-    stats: {
-      colors: true,
-    }
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+      target: 'http://localhost:8080'
+    });
   });
-
-  const bundlePath = path.join(__dirname, 'dist/index.html');
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-
-  app.get('*', function response(req, res) {
-    res.render('index.html')
-    res.end();
-  });
-
-
 }
 
-server.listen(port, () => {
-  console.log(`Express server has started on port:${port}`);
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
 });
-/*
-  // testing webpack dev server's code
-  app.use(express.static('dist'))
-  const route = (() => {
-    app.get('/', (req, res) => {
-      res.render('index.html');
-    })
-  })();
 
-  let server = app.listen(port, () => {
-    console.log(`Express server has started on port:${port}`);
-  });
-
-*/
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
+});
